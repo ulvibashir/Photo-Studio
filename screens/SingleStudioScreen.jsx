a2a34a5d93857213d54bc13af82688b5d3aed85c
Ulvi <ubashirov@outlook.com>
2020-07-02 14:09:26 +0400

    tst

--- screens/SingleStudioScreen.jsx
+++ screens/SingleStudioScreen.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
-import { CustomText as Text } from '../components';
-
-export const SingleStudioScreen = ({route: {params: {item}}}) => {
-  return(
-      <View>
-          <Text>
-            {item?.studioName}
-          </Text>
+import React, { useState } from "react";
+import { View, StyleSheet, FlatList, Image, Dimensions, TouchableOpacity } from "react-native";
+import { CustomText as Text, WelcomeScrIndicators } from "../components";
+import uuid from 'react-uuid'
+export const SingleStudioScreen = ({
+  route: {
+    params: { item: studio },
+  },
+}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgPart}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={studio.imgArray}
+          renderItem={({ item }) => {
+            return <Image style={styles.img} source={{ uri: item }} />;
+          }}
+          horizontal={true}
+          keyExtractor={() => uuid()}
+        />
       </View>
-  )
-}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imgPart: {
+    width: "100%",
+    height: 350,
+  },
+  img: {
+    width: Dimensions.get("window").width,
+  },
+});
